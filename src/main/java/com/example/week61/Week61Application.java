06a3bbd1f35c386082da52f7934717015f149f87
Hotelcombiend <63070141@it.kmitl.ac.th>
2022-10-04 09:27:31 +0700

    week6

---
+++ src/main/java/com/example/week61/Week61Application.java
@@ -0,0 +1,13 @@
+package com.example.week61;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week61Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week61Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/week61/Week61ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.week61;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week61ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
